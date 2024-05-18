f2637d5272f3e9da48a5aa49cbe691230a20ae16
Jeffrey <j.lo128456@gmail.com>
2024-05-18 21:01:10 +1000

    completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var  customerName = "bob";
+const leastFavoriteCustomer = "greg";
+//Uppercase customer name
+function upperCaseCustomerName()
+{
+    return customerName = customerName.toUpperCase();
+}
+//set best customer function
+function setBestCustomer()
+{
+    bestCustomer = "not bob";
+}
+setBestCustomer(bestCustomer);
+// over write Best customer
+function overwriteBestCustomer()
+{
+    bestCustomer = "maybe bob"
+}
+overwriteBestCustomer();
+// Change least favourite cusomter solution
+function changeLeastFavoriteCustomer()
+{
+    leastFavoriteCustomer = "bob";
+}
+
+
